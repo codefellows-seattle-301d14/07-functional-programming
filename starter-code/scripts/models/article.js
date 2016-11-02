ad3b677cbcc213705337a2b28e45c50c41bb446a
Ben Petty <benjamin.s.petty@gmail.com>
2016-11-01 17:28:59 -0700

    allAuthors maps, returns author names

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -69,7 +69,7 @@
     // NOTE: Grab the word count from each article body.
       return article.body.split(' ').length;
     })
-    // TODO: complete this reduce to get a grand total word count
+    // TODO:DONE complete this reduce to get a grand total word count
     .reduce(function(acc, cur, idx, array) {
       acc = acc + cur;
       return acc;
@@ -79,13 +79,17 @@
   /* Chain together a `map` and a `reduce` call to
           produce an array of *unique* author names. */
   Article.allAuthors = function() {
-  //return       TODO: map our collection
-    //return    TODO: return just the author names
-
+  //return       TODO:DONE map our collection
+  return Article.allArticles.map(function(article) {
+    //return    TODO:DONE return just the author names
+    return article.author;
+    // console.log(article.author);
   /* TODO: For our reduce that we'll chain here -- since we are trying to
       return an array, we'll need to specify an accumulator type (AKA initial value)
       What should this accumulator be and where is it placed? */
+    });
   };
+
 
   Article.numWordsByAuthor = function() {
   /*  Transform each author element into an object with 2 properties:
--- starter-code/scripts/views/adminPage.js
+++ starter-code/scripts/views/adminPage.js
@@ -2,9 +2,8 @@ articleView.renderAdminPage = function() {
   var statsRender = Handlebars.compile($('#stats-template').html());
   $('#blog-stats .articles').text(Article.allArticles.length);
 
-  //TODO: uncomment when complete:
+  //TODO:DONE uncomment when complete:
   $('#blog-stats .words').text(Article.numWordsAll());
-
 
   /* TODO: uncomment when ready to test:
   Article.numWordsByAuthor().forEach(function(numWordsObj) {
